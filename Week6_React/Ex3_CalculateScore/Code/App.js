6b1508e29ddfb54c41c239c1a046be36e8c44bc3
yoga292 <yogapriyavision@gmail.com>
2025-07-27 11:41:08 +0530

    Add files via upload

---
+++ Week6_React/Ex3_CalculateScore/Code/App.js
@@ -0,0 +1,16 @@
+import { CalculateScore } from "./Components/CalculateScore";
+
+
+function App() {
+  return (
+    <div>
+      <CalculateScore Name = {"Shalini"}
+      School = {"Amalorpavam Higher Secondary School"}
+      total = {588}
+      goal = {600}
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ Week6_React/Ex4_BlogApp/Code/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Posts from './Posts';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Blog Application</h1>
+      <Posts />
+    </div>
+  );
+}
+
+export default App;
